97134e3ab52e078cff493d38bf83a96d61903734
Renato Carrasco <tatonro12361@gmail.com>
2024-11-07 23:10:51 -0500

    Crear / Cargar / Delete

---
+++ Clase Miercoles 06.11.24-07.11.24/BE/csItems.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class csItems
+    {
+        public string ItemCode { get; set; }
+        public string ItemName { get; set; }
+        public int GroupCode { get; set; }
+        public string InvntItem { get; set; }
+        public string SellItem { get; set; }
+        public string PrchseItem { get; set; }
+        public string U_C2410_P001 { get; set; }
+
+    }
+}
